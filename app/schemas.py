6ed74184bd513a488bb608f8baecbb3bd395711b
Rafał Lelito <rafal.lelito@onet.com.pl>
2026-01-17 19:44:23 +0100

    Adds Crud & Schemas

---
+++ app/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class User(BaseModel):
+    name: str
+    website: str
+    age: int
+    role: str
+
+class UserCreate(User):
+    password: str
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    website: Optional[str] = None
+    age: Optional[int] = None
+    role: Optional[str] = None
+
+class UserOut(User):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
